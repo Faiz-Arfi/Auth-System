80acfcbb1e89ccfbe465ce76e29fbff88a12cee2
Faiz Arfi <faizarfi12@gmail.com>
2025-08-18 02:06:58 +0530

    initial commit

---
+++ auth-system-backend/src/test/java/com/example/authsystembackend/AuthSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
