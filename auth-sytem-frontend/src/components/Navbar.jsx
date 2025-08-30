6ebc380c6d1967365920ed69358dd1509f2682fe
Faiz Arfi <faizarfi12@gmail.com>
2025-08-30 20:33:45 +0530

    replace a tags with navlinks and links

--- auth-sytem-frontend/src/components/Navbar.jsx
+++ auth-sytem-frontend/src/components/Navbar.jsx
@@ -1,25 +1,40 @@
-import React from "react";
 import Logo from "../assets/brand-logo.svg";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className="flex justify-around items-center p-4 bg-white shadow-md">
             {/* Brand */}
-            <div className="flex items-center gap-2">
+            <Link to="/" className="flex items-center gap-2">
                 <img src={Logo} alt="Brand Logo" className="w-8 h-8" />
                 <span className="font-bold text-xl">Auth-S</span>
-            </div>
+            </Link>
 
             {/* Navigation Links */}
             <ul className="flex gap-6 items-center">
-                <li><a href="/" className="hover:text-blue-600">Home</a></li>
-                <li><a href="/about" className="hover:text-blue-600">About</a></li>
-                <li><a href="/contact" className="hover:text-blue-600">Contact</a></li>
+                <li>
+                    <NavLink to="/" className={({ isActive }) =>
+                        isActive ? "text-green-600 font-semibold" : "hover:text-green-600"}>
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about" className={({ isActive }) =>
+                        isActive ? "text-green-600 font-semibold" : "hover:text-green-600"}>
+                        About
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/contact" className={({ isActive }) =>
+                        isActive ? "text-green-600 font-semibold" : "hover:text-green-600"}>
+                        Contact
+                    </NavLink>
+                </li>
                 
             </ul>
             {/* login and signup */}
             <ul className="flex gap-4 items-center">
-                <li className="bg-gray-800 hover:bg-gray-600 px-4 py-2 rounded-md"><a href="/login" className="text-amber-50">Login/Register</a></li>
+                <li className="bg-gray-800 hover:bg-gray-600 px-4 py-2 rounded-md"><Link to="/login" className="text-amber-50">Login/Register</Link></li>
             </ul>
         </nav>
     );
