b2066f1e16681e630f90ab833c293fa68a7f5ecb
Faiz Arfi <faizarfi12@gmail.com>
2025-08-30 20:34:43 +0530

    Routed all pages developed till now

--- auth-sytem-frontend/src/App.jsx
+++ auth-sytem-frontend/src/App.jsx
@@ -1,14 +1,43 @@
-import { useState } from 'react'
 import './App.css'
 import HomePage from './pages/HomePage'
 import Navbar from './components/Navbar'
+import AuthPage from './pages/AuthPage'
+import UserPage from './pages/UserPage'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ErrorPage from './pages/ErrorPage'
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <>
+        <Navbar />
+        <HomePage />
+      </>,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/login",
+      element: <>
+        <Navbar />
+        <AuthPage />
+      </>,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/user",
+      element: <>
+        <Navbar />
+        <UserPage />
+      </>,
+      errorElement: <ErrorPage />,
+    },
+  ]);
+
   return (
     <>
-      <Navbar />
-      <HomePage />
+      <RouterProvider router={router} />
     </>
   )
 }
