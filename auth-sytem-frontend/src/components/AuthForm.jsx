043bf93ca86273b50227b344480b6c290db5aca6
Faiz Arfi <faizarfi12@gmail.com>
2025-08-30 16:50:21 +0530

    initial integration of login/register page

---
+++ auth-sytem-frontend/src/components/AuthForm.jsx
@@ -0,0 +1,94 @@
+import { useState } from 'react'
+import GoogleLogo from '../assets/google-icon-logo.svg'
+import ShowPassIcon from '../assets/password-show.svg'
+import HidePassIcon from '../assets/password-hide.svg'
+
+const AuthForm = ( { mode = "register"} ) => {
+    const [showPass, setShowPass] = useState(false);
+    const isLogin = mode === "login";
+    
+    return (<>
+        
+            {isLogin ?
+            <div className='action-text w-full text-center'>
+                <h1 className='text-4xl font-bold mb-2 '>Welcome Back,</h1>
+                <p className='text-gray-600'>Please Log-in to continue</p>
+            </div>
+            :
+            <div className='action-text w-full text-center'>
+                <h1 className='text-4xl font-bold mb-2 '>Are you new User?</h1>
+                <p className='text-gray-600'>Please Register yourself</p>
+            </div>
+        }
+            
+            <div className="goole-auth w-full">
+                <a className='w-full flex items-center justify-center gap-3 border-2 border-gray-400 px-5 py-3 rounded-md hover:cursor-pointer hover:bg-gray-100 transition'>
+                    <img src={GoogleLogo} alt="google-logo" className='w-6 h-6' />
+                    <span>Continue with Google</span>
+                </a>
+            </div>
+            <form className='flex flex-col gap-4 w-full'>
+                <h2 className="grid grid-cols-[1fr_auto_1fr] items-center gap-x-6">
+                    <span className="block h-px bg-gray-400" />
+                    <span className='text-gray-400'>Or</span>
+                    <span className="block h-px bg-gray-400" />
+                </h2>
+                {isLogin ? null :
+                    <div className='relative w-full'>
+                    <label
+                        htmlFor="username"
+                        className="absolute -top-2 left-3 bg-white px-1 text-sm text-gray-600"
+                    >
+                        Username
+                    </label>
+                    <input
+                        type='text'
+                        id='username'
+                        className='border border-gray-400 rounded px-4 py-3 w-full focus:outline-none focus:border-green-500'
+                    />
+                </div>
+                }
+                
+                <div className='relative w-full'>
+                    <label
+                        htmlFor='email'
+                        className='absolute -top-2 left-4 bg-white text-sm text-gray-600'
+                    >
+                        Email
+                    </label>
+                    <input
+                        type='email'
+                        id='email'
+                        className='border border-gray-400 rounded px-4 py-3 w-full focus:outline-none focus:border-green-500'
+                    />
+                </div>
+                <div className="relative w-full">
+                    <label
+                        htmlFor='password'
+                        className='absolute -top-2 left-4 bg-white text-sm text-gray-600'
+                    >
+                        Password
+                    </label>
+                    <input
+                        type={showPass ? 'text' : 'password'}
+                        id='password'
+                        className='border border-gray-400 rounded px-4 py-3 pr-10 w-full focus:outline-none focus:border-green-500'
+                    />
+                    <button
+                        type='button'
+                        onClick={() => setShowPass(!showPass)}
+                        className='absolute right-3 top-1/2 -translate-y-1/2 hover:cursor-pointer'>
+                        <img src={showPass ? ShowPassIcon : HidePassIcon} alt="show password" className='w-6' />
+                    </button>
+                </div>
+                {isLogin ?
+                    <button type="submit" className='bg-gray-800 text-white px-5 py-3 rounded-md hover:bg-gray-600 transition'>Log-in</button>
+                    :
+                    <button type="submit" className='bg-gray-800 text-white px-5 py-3 rounded-md hover:bg-gray-600 transition'>Register</button>
+            }
+            </form>
+    </>
+    )
+}
+
+export default AuthForm
---
+++ auth-sytem-frontend/src/pages/AuthPage.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+import AuthForm from '../components/AuthForm'
+
+const AuthPage = () => {
+
+    const [isLogin, setIsLogin] = useState(false);
+
+
+    return (<>
+        <div className='mt-4 auth-page flex flex-col justify-center items-center gap-6 p-6 w-2/6 m-auto'>
+            <div className='action-btn flex gap-4 w-full'>
+                {isLogin ? 
+                <button 
+                    onClick={() => setIsLogin(false)}
+                    className='
+                        flex-1 bg-white text-black border-2 border-gray-600 py-2 rounded-md hover:bg-gray-800 hover:text-white transition'>Sign up</button>
+                     :
+                    <button 
+                    onClick={() => setIsLogin(false)}
+                    className='
+                        flex-1 bg-gray-800 text-white py-2 rounded-md hover:bg-gray-600 transition'>Sign up</button>
+                }
+                {isLogin ? 
+                <button 
+                    onClick={() => setIsLogin(true)}
+                    className='
+                        flex-1 bg-gray-800 text-white py-2 rounded-md hover:bg-gray-600 transition'>Log In</button>
+                        :
+                <button 
+                    onClick={() => setIsLogin(true)}
+                    className='
+                        flex-1 bg-white text-black border-2 border-gray-600 py-2 rounded-md hover:bg-gray-800 hover:text-white transition'>Log In</button>
+                }
+                
+            </div>
+            <AuthForm mode={isLogin ? "login" : "register"} />
+        </div>
+    </>
+    )
+}
+
+export default AuthPage
