446b0c16b41024d57f536696044a8bbfae531396
Faiz Arfi <faizarfi12@gmail.com>
2025-08-30 20:36:34 +0530

    initial stage of userpage

---
+++ auth-sytem-frontend/src/pages/UserPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const UserPage = () => {
+  return (<>
+    <div className="stats">
+        <div className="stat">
+            Registered On:
+            <div className="stat-value">31/08/2025</div>
+        </div>
+        <div className="stat">
+            Last Log-in:
+            <div className="stat-value">31/08/2025</div>
+        </div>
+        <div className="stat">
+            Number of Log-ins:
+            <div className="stat-value">1</div>
+        </div>
+    </div>
+  </>
+  )
+}
+
+export default UserPage
