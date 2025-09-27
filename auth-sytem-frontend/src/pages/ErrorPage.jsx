190a5ff0c014c0e1175e816ab5d35db10b4d74b7
Faiz Arfi <faizarfi12@gmail.com>
2025-09-27 21:24:38 +0530

    added back link to home page for the error page

--- auth-sytem-frontend/src/pages/ErrorPage.jsx
+++ auth-sytem-frontend/src/pages/ErrorPage.jsx
@@ -5,6 +5,7 @@ const ErrorPage = () => {
     <div className='min-h-screen flex flex-col justify-center items-center'>
         <h1 className='text-4xl text-center'>404 - Page Not Found</h1>
         <p className='text-center mt-4'>The page you are looking for does not exist.</p>
+        <a href='/' className='mt-6 text-blue-500 underline'>Go back to Home</a>
     </div>
   )
 }
