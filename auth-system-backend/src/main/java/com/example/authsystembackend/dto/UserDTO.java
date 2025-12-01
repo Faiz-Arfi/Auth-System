c29c97ddfc24e534c603b483888c63ed0fd4349e
Faiz Arfi <faizarfi12@gmail.com>
2025-12-02 01:37:48 +0530

    changed the user dto to return all the feilds required by the frontend

--- auth-system-backend/src/main/java/com/example/authsystembackend/dto/UserDTO.java
+++ auth-system-backend/src/main/java/com/example/authsystembackend/dto/UserDTO.java
@@ -1,10 +1,13 @@
 package com.example.authsystembackend.dto;
 
+import com.example.authsystembackend.entity.Role;
 import com.example.authsystembackend.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
@@ -15,6 +18,16 @@ public class UserDTO {
     private String userName;
     private String email;
     private boolean isEmailVerified;
+    private boolean activity1Status;
+    private boolean activity2Status;
+    private boolean activity3Status;
+    private boolean activity4Status;
+    private boolean activity5Status;
+    private Integer points;
+    private Date previousLogin;
+    private Date createdAt;
+    private Integer noOfLogins;
+    private String role;
 
     public User toEntity(UserDTO userDTO) {
         return User.builder()
@@ -22,6 +35,16 @@ public class UserDTO {
                 .userName(userDTO.getUserName())
                 .email(userDTO.getEmail())
                 .isEmailVerified(userDTO.isEmailVerified())
+                .activity1Status(userDTO.isActivity1Status())
+                .activity2Status(userDTO.isActivity2Status())
+                .activity3Status(userDTO.isActivity3Status())
+                .activity4Status(userDTO.isActivity4Status())
+                .activity5Status(userDTO.isActivity5Status())
+                .points(userDTO.getPoints())
+                .previousLogin(userDTO.getPreviousLogin())
+                .createdAt(userDTO.getCreatedAt())
+                .noOfLogins(userDTO.getNoOfLogins())
+                .role(Role.valueOf(userDTO.getRole()))
                 .build();
     }
 
@@ -31,6 +54,16 @@ public class UserDTO {
                 .email(user.getEmail())
                 .userName(user.getUserName())
                 .isEmailVerified(user.isEmailVerified())
+                .activity1Status(user.isActivity1Status())
+                .activity2Status(user.isActivity2Status())
+                .activity3Status(user.isActivity3Status())
+                .activity4Status(user.isActivity4Status())
+                .activity5Status(user.isActivity5Status())
+                .points(user.getPoints())
+                .createdAt(user.getCreatedAt())
+                .previousLogin(user.getPreviousLogin())
+                .noOfLogins(user.getNoOfLogins())
+                .role(user.getRole().toString())
                 .build();
     }
 }
