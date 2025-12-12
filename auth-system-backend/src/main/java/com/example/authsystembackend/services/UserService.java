fa3658eca5f38581dc8892a401b6a9b65263f023
Faiz Arfi <faizarfi12@gmail.com>
2025-12-13 00:33:48 +0530

    Added activity log for google oAuth login

--- auth-system-backend/src/main/java/com/example/authsystembackend/services/UserService.java
+++ auth-system-backend/src/main/java/com/example/authsystembackend/services/UserService.java
@@ -1,17 +1,17 @@
 package com.example.authsystembackend.services;
 
+import com.example.authsystembackend.entity.ActivityLog;
 import com.example.authsystembackend.entity.Role;
 import com.example.authsystembackend.entity.User;
 import com.example.authsystembackend.repository.UserRepo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.sql.Timestamp;
 import java.util.Optional;
 
 @Service
@@ -29,13 +29,16 @@ public class UserService {
         return userRepo.findByEmail(email).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
     }
 
+    @Transactional
     public User processOAuthPostLogin(String email, String name, String pictureUrl) {
         Optional<User> existingUser = userRepo.findByEmail(email);
         if (existingUser.isPresent()) {
             User user = existingUser.get();
             user.setProfilePicture(pictureUrl);
             user.setNoOfLogins(user.getNoOfLogins() + 1);
-            return userRepo.save(user);
+
+            //Log the activity
+            return getUser(user);
         }
         User newUser = User.builder()
                 .email(email)
@@ -53,7 +56,22 @@ public class UserService {
                 .activity5Status(false)
                 .noOfLogins(1)
                 .build();
-        return userRepo.save(newUser);
+
+        return getUser(newUser);
+    }
+
+    private User getUser(User user) {
+        ActivityLog activityLog = ActivityLog.builder()
+                .recordedAt(new java.sql.Timestamp(System.currentTimeMillis()))
+                .type(ActivityLog.ActivityType.LOGIN)
+                .severity(ActivityLog.ActivitySeverity.MODERATE)
+                .description("Log-in Using Google OAuth")
+                .user(user)
+                .build();
+        if(user.getActivityLogs() == null) user.setActivityLogs(new java.util.ArrayList<>());
+        user.getActivityLogs().add(activityLog);
+
+        return userRepo.save(user);
     }
 
     private CharSequence generateRandomPassword() {
@@ -68,6 +86,16 @@ public class UserService {
     public ResponseEntity<?> changePassword(String email, String newPassword) {
         User user = getUserByEmail(email);
         user.setPassword(passwordEncoder.encode(newPassword));
+
+        //Log the activity
+        ActivityLog activityLog = ActivityLog.builder()
+                        .user(user)
+                                .type(ActivityLog.ActivityType.PASSWORD_CHANGE)
+                                        .severity(ActivityLog.ActivitySeverity.ATTENTION_REQUIRED)
+                                                .description("Password Changed")
+                                                        .recordedAt(new Timestamp(System.currentTimeMillis()))
+                                                                .build();
+        user.getActivityLogs().add(activityLog);
         userRepo.save(user);
         return ResponseEntity.ok().build();
     }
