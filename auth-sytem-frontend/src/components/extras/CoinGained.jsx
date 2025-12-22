7bbd54793b227b957c11a93d9661eb678224b954
Faiz Arfi <faizarfi12@gmail.com>
2025-12-23 00:04:39 +0530

    Component to show the activity completion modal

---
+++ auth-sytem-frontend/src/components/extras/CoinGained.jsx
@@ -0,0 +1,44 @@
+import { Coins, X } from 'lucide-react';
+import React from 'react'
+
+const CoinGained = ({
+    coinValue = 500,
+    onClose = null
+}) => {
+
+  return (
+    <div className="fixed inset-0 bg-transparent backdrop-blur-sm flex items-center justify-center z-50 p-4">
+            <div className="bg-white rounded-2xl shadow-2xl max-w-md w-full p-6 relative animation-fadeIn">
+
+                <button
+                    onClick={onClose}
+                    className='absolute top-4 right-4 text-gray-400 hover:text-gray-600 transition-colors'
+                >
+                    <X className='w-6 h-6' />
+                </button>
+
+                <div className="flex justify-center mb-4">
+                    <div className={`bg-green-100 p-4 rounded-full`}>
+                        <Coins className={`w-12 h-12 text-green-700`} />
+                    </div>
+                </div>
+
+                <h2 className='text-2xl font-bold text-gray-800 text-center mb-2'>
+                    You have gained <span className='text-green-600'>{coinValue}</span> Points!
+                </h2>
+                <p className='text-gray-600 text-center mb-6'>
+                    Congrats you have just completed an activity and earned some points. Keep up the great work!
+                </p>
+                <div className="flex justify-center">
+                    <button
+                        onClick={onClose}
+                        className="bg-green-600 text-white px-6 py-3 rounded-lg font-semibold hover:bg-green-700 transition-colors"
+                    > Got It</button>
+                </div>
+                
+            </div>
+        </div>
+  )
+}
+
+export default CoinGained
